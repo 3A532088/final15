2ec7813757648985ae40e33e01fabd2630b667b8
李建德 <ah247517m@gmail.com>
2018-12-14 14:13:32 +0800

    bootstaps 套入並建立樣板

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>
+</title>
+</head>
+<body>
+<div class="container">
+     @include('layouts.partials.navigation')
+</div>
+@include('layouts.partials.footer')
+</body>
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    <script src={{asset('js/jquery/jquery-2.2.4.min.js')}}></script>
+    <!-- Popper js -->
+    <script src={{asset('js/popper.min.js')}}></script>
+    <!-- Bootstrap js -->
+    <script src={{asset('js/bootstrap.min.js')}}></script>
+    <!-- Plugins js -->
+    <script src={{asset('js/plugins.js')}}></script>
+    <!-- Classy Nav js -->
+    <script src={{asset('js/classy-nav.min.js')}}></script>
+    <!-- Active js -->
+    <script src={{asset('js/active.js')}}></script>
+</html>
