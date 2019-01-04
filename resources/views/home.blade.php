82401b507871b5359028971c4e849e332210686a
李建德 <ah247517m@gmail.com>
2019-01-04 15:33:26 +0800

    結帳完後刪除carts資料庫對應的資料

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -87,7 +87,7 @@
             </div>
 
             <div class="card">
-                <div class="card-header"style="text-align:center">{{ __('個人資料') }}</div>
+                <div class="card-header"style="text-align:center">{{ __('訂單資料') }}</div>
 
 
                 <div class="card-body">
