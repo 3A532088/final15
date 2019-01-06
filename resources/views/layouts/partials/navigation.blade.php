39aec09680bc828e8b8c2b23b327c348aee6bcc1
吳駿松 <jo32229hn@gmail.com>
2019-01-07 07:52:41 +0800

    左上planting 版面css微調

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -3,7 +3,7 @@
             <!-- Classy Menu -->
             <nav class="classy-navbar" id="essenceNav">
                 <!-- Logo -->
-                <a class="nav-brand" href="{{route('main.home')}}"><img src="{{asset('img/core-img/logoplants12.png')}}" alt=""></a>
+                <a class="nav-brand mb-1" href="{{route('main.home')}}"><img src="{{asset('img/core-img/logoplants12.png')}}" alt=""></a>
                 <!-- Navbar Toggler -->
                 <div class="classy-navbar-toggler">
                     <span class="navbarToggler"><span></span><span></span><span></span></span>
