241ac868e03a0d4f46a3a73415369cc89b2b5bbc
吳駿松 <jo32229hn@gmail.com>
2019-01-07 07:49:26 +0800

    新聞詳細頁面的 view route controller

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -19,7 +19,6 @@
     <link rel="stylesheet" href="../css/core-style.css">
     <link rel="stylesheet" href="../style.css">
 
-
 </head>
 
 <body>
@@ -41,90 +40,28 @@
     <div class="blog-wrapper section-padding-80">
         <div class="container">
             <div class="row">
-               
+            @foreach($news as $new)
                 <!-- Single Blog Area -->
                 <div class="col-12 col-lg-6">
                     <div class="single-blog-area mb-50">
-                        <img src="img/news-img/news1.jpg" alt="">
+                        <img src="img/news-img/{{$new->photo}}" alt="">
                         <!-- Post Title -->
                         <div class="post-title">
-                            <a href="#">為保護生物多樣性 李家維買下苗栗1.2萬平方米山頭</a>
+                            <a href="#">{{$new->title}}</a>
                         </div>
                         <!-- Hover Content -->
                         <div class="hover-content">
                             <!-- Post Title -->
                             <div class="hover-post-title">
-                                <a href="#">為保護生物多樣性 李家維買下苗栗1.2萬平方米山頭</a>
+                                <a href="#">{{$new->title}}</a>
                             </div>
-                            <p>「我想讓自然界重新在這裡，發展出低海拔森林該有的樣子。」去年底墜谷靠「海底雞」 罐頭保命的清華大學生命科學系教授李家維，30年前鑿於苗栗縣南庄鄉獅山村海拔300多公尺的山區動植物優美，想要保護這裡的生物多樣性，而花330萬買下周遭1.2萬平方公尺的山林。</p>
-                            <a href="{{route('news.detail')}}">繼續閱讀<i class="fa fa-angle-right"></i></a>
+                            <p>{{$new->content1}}</p>
+                            <a href="{{route('news.detail',['id'=>$new->id])}}">繼續閱讀<i class="fa fa-angle-right"></i></a>
                         </div>
                     </div>
                 </div>
-               
-                <!-- Single Blog Area -->
-                <div class="col-12 col-lg-6">
-                    <div class="single-blog-area mb-50">
-                        <img src="img/news-img/news_2.jpg" alt="">
-                        <!-- Post Title -->
-                        <div class="post-title">
-                            <a href="#">你想要在家裡種花種菜，但是又擔心沒時間照顧嗎？</a>
-                        </div>
-                        <!-- Hover Content -->
-                        <div class="hover-content">
-                            <!-- Post Title -->
-                            <div class="hover-post-title">
-                                <a href="#">你想要在家裡種花種菜，但是又擔心沒時間照顧嗎？</a>
-                            </div>
+            @endforeach
 
-                            <p>美國舊金山一間新創公司推出了智慧型盆栽，內建LED日照燈和儲水槽，只要插上電就能種出美麗植物。在辦公桌上放個盆栽很療癒，但很多人工作一忙就忘記澆水施肥，舊金山一間新創公司推出智慧型盆栽，再忙的人也不用擔心養死植物。</p>
-                            <a href="{{route('news.detail')}}">繼續閱讀<i class="fa fa-angle-right"></i></a>
-
-                        </div>
-                    </div>
-                </div>
-               
-                <!-- Single Blog Area -->
-                <div class="col-12 col-lg-6">
-                    <div class="single-blog-area mb-50">
-                        <img src="img/news-img/news3.jpg" alt="">
-                        <!-- Post Title -->
-                        <div class="post-title">
-                            <a href="#">上一次是2014年 「屍花」綻放飄濃郁腐肉味！</a>
-                        </div>
-                        <!-- Hover Content -->
-                        <div class="hover-content">
-                            <!-- Post Title -->
-                            <div class="hover-post-title">
-                                <a href="#">上一次是2014年 「屍花」綻放飄濃郁腐肉味！</a>
-                            </div>
-                            <p>美國南加州杭廷頓圖書館（Huntington Library）一株巨花魔芋開花了，稀有的樣貌與獨特腐肉氣味，讓粉絲相當興奮。</p>
-                            <a href="{{route('news.detail')}}">繼續閱讀<i class="fa fa-angle-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-               
-                <!-- Single Blog Area -->
-                <div class="col-12 col-lg-6">
-                    <div class="single-blog-area mb-50">
-                        <img src="img/news-img/news4.jpg" alt="">
-                        <!-- Post Title -->
-                        <div class="post-title">
-                            <a href="#">香蕉、鳳梨竟是外來種！專家：台灣植物原生種很少</a>
-                        </div>
-                        <!-- Hover Content -->
-                        <div class="hover-content">
-                            <!-- Post Title -->
-                            <div class="hover-post-title">
-                                <a href="#">香蕉、鳳梨竟是外來種！專家：台灣植物原生種很少</a>
-                            </div>
-                            <p>台灣物產豐饒，但你知道很多台灣盛產的水果其實來自其他國家嗎？植物蒐藏家王瑞閔表示，台灣原生植物實際上很少，大家認為的台灣特產水果鳳梨、香蕉其實都是外來種。</p>
-                            <a href="{{route('news.detail')}}">繼續閱讀<i class="fa fa-angle-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-               
-                
             </div>
         </div>
     </div>
